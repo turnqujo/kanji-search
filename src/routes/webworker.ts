e1b2542335f337d4cdd003970ca2e176da55eaf5
turnqujo <turnquist.joseph@mailbox.org>
2019-12-29 15:41:44 -0800

    Added conversion from TypeScript to JavaScript for webworkers

--- src/routes/webworker.ts
+++ src/routes/webworker.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
 import fs from 'fs'
+import { transpile } from 'typescript'
 
 const webworkerRoutes = Router()
+
+function convertToJavaScript(typeScriptCode: Buffer): Buffer {
+  const transpiled = transpile(typeScriptCode.toString())
+  return Buffer.alloc(transpiled.length, transpiled)
+}
 
 webworkerRoutes.get('/all-kanji', (_, res) =>
   fs.readFile('src/webworkers/getAllKanji.ts', (err, data) =>
     !!err
-      ? res.status(500).send(err)
-      : res.send(data)
+      ? res.status(500).send('Something went wrong.')
+      : res.send(convertToJavaScript(data))
   )
 )
 
