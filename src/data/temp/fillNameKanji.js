4b0e13b65063d3c49704bba014be7bb09220b9c1
Joseph Turnquist <turnquist.joseph@mailbox.org>
2020-01-29 09:19:06 -0800

    Got the name kanji (Jinmeiyoo) together. Could use some more work, but this should be fine. Next up is to deliver and store the kanji in an IndexedDB table.

--- src/data/temp/fillNameKanji.js
+++ src/data/temp/fillNameKanji.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
 const allKanji = JSON.parse(fs.readFileSync('../all_kanji.json', 'utf8'))
-const jinmeiyoo = JSON.parse(fs.readFileSync('../jinmeiyoo.json', 'utf8'))
+const jinmeiyoo = JSON.parse(fs.readFileSync('./jinmeiyoo_raw.json', 'utf8'))
 const joyoVariants = JSON.parse(fs.readFileSync('./joyoVariants.json', 'utf8'))
 
 const foundKanji = [] // Expected to be full kanji objects
-const missingKanji = [] // Just the kanji
+const unknownKanji = [] // Just the kanji
 
 let numJoyoVariants = 0
 
@@ -14,63 +14,78 @@ for(let jinmeiyooKanji of jinmeiyoo) {
     const joyoVariant = joyoVariants.find(x => x[0] === jinmeiyooKanji)
 
     if (!!joyoVariant) {
-      if (jinmeiyooKanji === '祕') {
-        console.log(`HERE! ${jinmeiyooKanji}, ${JSON.stringify(joyoVariant)}`)
-      }
-      
       numJoyoVariants++
       jinmeiyooKanji = joyoVariant
     }
   }
 
-  let found
-  let variants = []
-
   if (typeof jinmeiyooKanji === 'string') {
-    found = allKanji.find(x => x.char === jinmeiyooKanji)
-  }
+    const foundChar = allKanji.find(x => x.char === jinmeiyooKanji)
+    if (!!foundChar) {
+      foundKanji.push({...foundChar, variants: []})
+      continue
+    }
 
-  if (Array.isArray(jinmeiyooKanji)) {
-    found = allKanji.find(x => x.char === jinmeiyooKanji[0] || x.char === jinmeiyooKanji[1])
-    variants = [jinmeiyooKanji[1]]
-  }
-
-  if (!!found) {
-    foundKanji.push({...found, variants})
+    unknownKanji.push(jinmeiyooKanji)
     continue
   }
 
-  missingKanji.push(jinmeiyooKanji)
+  if (Array.isArray(jinmeiyooKanji)) {
+    const foundChar = allKanji.find(x => x.char === jinmeiyooKanji[0])
+    const foundVariation = allKanji.find(x => x.char === jinmeiyooKanji[1])
+
+    if (!!foundChar && !!foundVariation) {
+      foundKanji.push({...foundChar, variants: [jinmeiyooKanji[1]]})
+
+      // TODO: add this one also? Maybe form a new kanji definition with merged data?
+      foundKanji.push({...foundVariation, variants: [jinmeiyooKanji[0]]})
+      continue
+    }
+
+    if (!!foundChar) {
+      foundKanji.push({...foundChar, variants: [jinmeiyooKanji[1]]})
+      continue
+    }
+
+    if (!!foundVariation) {
+      foundKanji.push({...foundVariation, variants: [jinmeiyooKanji[0]]})
+      continue
+    }
+
+    unknownKanji.push(jinmeiyooKanji)
+    continue
+  }
+
+  console.error(`Kanji is of invalid type: ${typeof jinmeiyooKanji}\n\r${JSON.stringify(jinmeiyooKanji)}`)
 }
 
-fs.writeFileSync('./out.json', JSON.stringify(foundKanji, null, 2), 'utf8')
-fs.writeFileSync('./unknown.json', JSON.stringify(missingKanji, null, 2), 'utf8')
+fs.writeFileSync('./out.json', JSON.stringify(foundKanji), 'utf8')
+fs.writeFileSync('./unknown.json', JSON.stringify(unknownKanji, null, 2), 'utf8')
 
 console.log(`
   Found Kanji: ${foundKanji.length}
-  Unknown Kanji: ${missingKanji.length}
+  Unknown Kanji: ${unknownKanji.length}
   Joyo Variants: ${joyoVariants.length}
 `)
 
-/** TODO: There seems to be a problem with joyo variant kanji. Both the "char" and "variant"
- *        are somehow being set to the "variant", but only for some characters.
- * 
- *  NOTE: Found out that 祕 uses a variation of 秘, but these Kanji are both in the allKanji set.
- *        Not sure what the implications of this are.
- * 
-  {
-    "char": "秘",
-    "stroke": "10",
-    "meanings": [
-      "secret",
-      "conceal"
-    ],
-    "readings": [
-      "ヒ"
-    ],
-    "variants": [
-      "秘"
-    ]
-  },
- * 
- */
+// Test output data
+const output = JSON.parse(fs.readFileSync('./out.json', 'utf8'))
+const duplicateKanji = []
+const uniqueKanji = output.reduce((acc, curr) => {
+  if (acc.indexOf(curr.char) === -1) {
+    acc.push(curr.char)
+    return acc
+  }
+
+  duplicateKanji.push(curr)
+
+  return acc
+}, [])
+
+if (duplicateKanji.length > 0) {
+  console.error(`Duplicate kanji found:\r\n${JSON.stringify(duplicateKanji)}`)
+}
+
+if (output.length < 863) {
+  console.error(`Missing ${863 - output.length} kanji from the official list.`)
+}
