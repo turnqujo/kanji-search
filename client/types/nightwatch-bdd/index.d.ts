e9670483fa094d2acf6023aa53e51218788c2c9d
turnqujo <turnquist.joseph@mailbox.org>
2020-07-04 09:31:37 -0700

    Minor adjustments to the nightwatch bdd kludge

--- client/types/nightwatch-bdd/index.d.ts
+++ client/types/nightwatch-bdd/index.d.ts
@@ -1,9 +1,13 @@
 // TODO: This is being copy-pasted into node_modules/@types/nightwatch manually.
 // How to include this file?
 declare global {
+    // @ts-ignore
     function describe(description: string, cb: (browser: NightwatchBrowser) => void): void
+    // @ts-ignore
     function before(cb: (browser: NightwatchBrowser) => void): void
+    // @ts-ignore
     function after(cb: (browser: NightwatchBrowser) => void): void
+    // @ts-ignore
     function test(description: string, cb: (browser: NightwatchBrowser) => void): void
 }
 
